f202f75c826945c2a01ac1468b3b67fd8bc88906
Nicolas RAMOS <nramos@immonot.com>
2024-11-06 17:25:09 +0100

    :sparkles: Add class to get a list of objects indexed

---
+++ src/Indexer/IndexableObjects.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nramos\SearchIndexer\Indexer;
+
+class IndexableObjects
+{
+    public function __construct(
+        private readonly array $indexedClasses
+    )
+    {
+    }
+
+    public function getIndexedClasses(): array
+    {
+        return $this->indexedClasses;
+    }
+
+
+
+}
